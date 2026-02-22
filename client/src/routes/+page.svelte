764d724cd002aaa3f52efcaa99f41274c613db43
ayakase <wibu1892001@gmail.com>
2026-02-22 23:19:18 +0700

    feat: add initial SvelteKit home page and detailed project README.

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    let count = $state(0);
+
+    function increment() {
+        count += 1;
+    }
+</script>
+
+<div>
+    <h1>Home</h1>
+    <p>{count}</p>
+    <button onclick={increment}>Increment</button>
+</div>
